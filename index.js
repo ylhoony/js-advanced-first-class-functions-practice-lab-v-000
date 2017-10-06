81efa26c52269a6386cae623098b2a320bb31457
Hoon Lee <yl.hoony@gmail.com>
2017-10-06 09:10:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+
+const logDriverNames = (collection) => {
+  collection.forEach(function(driver) {
+    console.log(driver.name);
+  });
+}
+
+const logDriversByHometown = (collection, location) => {
+  collection.forEach(function(driver) {
+    if(driver.hometown === location) {
+      console.log(driver.name);
+    }
+  });
+}
+
+const driversByRevenue = (collection) => {
+  const newCollection = collection.sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+  return newCollection;
+}
+
+const driversByName = (collection) => {
+  const newCollection = collection.sort(function(a, b) {
+    return (a.name).localeCompare(b.name);
+  });
+  return newCollection;
+}
+
+const totalRevenue = (collection) => {
+  const result = collection.reduce((sum, value) => {
+    return sum + value.revenue;
+  }, 0);
+  return result;
+}
+
+const averageRevenue = (collection) => {
+  const result = collection.reduce((sum, value) => {
+    // console.log(sum, i);
+    return sum + value.revenue;
+  }, 0)
+  return result / (collection.length);
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -62,7 +62,7 @@ describe('index.js', function () {
       expect(spy.calledWithExactly('Annette')).to.not.be.true;
     });
   });
-  
+
   describe('driversByRevenue()', function () {
     it('uses the sort() method to return a new array of drivers ordered by revenue (lowest to highest)', function () {
       expect(driversByRevenue(drivers)[0].name).to.eql('Sally');
